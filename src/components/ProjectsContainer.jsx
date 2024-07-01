d5d2a741d75149e1d6744be304cd9b42a9ef472d
Carlos Fermino <brutosacf@gmail.com>
2024-07-01 20:19:37 -0300

    Correção da resposividade dos title e dos cards

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,7 +3,7 @@ const ProjectsContainer = () => {
     <section className="projects-container">
       <h2>Projetos</h2>
       <p>
-        Este e um projetos desenvolvidos durante a minha transição de carreira de Quality Assurance para Desenvolvedor web FrontEnd, BackEnd Full Stack. 
+        Este e um dos projetos desenvolvidos durante a minha transição de carreira de Quality Assurance para Desenvolvedor web FrontEnd, BackEnd Full Stack. 
         Este site disponibiliza a previsão do tempo com base na cidade que o usuário digitar.
         Tecnologias do projeto: HTML, CSS, Javascript, Git,ReactJs, NextJs, NodeJs, Styled Components e API.
 
@@ -13,7 +13,7 @@ const ProjectsContainer = () => {
       </a>
       <br/>
       <p>
-        Este e um projetos desenvolvidos durante a minha transição de carreira de Quality Assurance para Desenvolvedor web FrontEnd, BackEnd Full Stack. Este site disponibiliza uma forma de comunicação interna entre  pequenos grupos de pessoas, de uma empresa ou escola. HTML, CSS, Javascript, Git,ReactJs, NextJs, NodeJs, Styled Components, NextJs, Express,Webpack, Token e JWT e MongoDB
+        Este e mais um projetos desenvolvidos durante a minha transição de carreira de Quality Assurance para Desenvolvedor web FrontEnd, BackEnd Full Stack. Este site disponibiliza uma forma de comunicação interna entre  pequenos grupos de pessoas, de uma empresa ou escola. HTML, CSS, Javascript, Git,ReactJs, NextJs, NodeJs, Styled Components, NextJs, Express,Webpack, Token e JWT e MongoDB
 
       </p>
       <a href="https://social-dev-opal.vercel.app/login" target="_blank" className="btn">
