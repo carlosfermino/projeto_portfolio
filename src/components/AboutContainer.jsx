b973832fecc16b5efa3dee47d435fc8d3421440b
Carlos Fermino <brutosacf@gmail.com>
2024-12-06 17:51:10 -0300

    Correção dos campos sobre e descrição do projeto

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,7 +3,10 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-      Sou apaixonado por tecnologia e estou em processo de transição de carreira de Quality Assurance para Desenvolvedor web FrontEnd, BackEnd Full Stack. Gosto de me envolver em projetos de vários setores e quero fazer parte do seu time.
+        Sou apaixonado por tecnologia com uma mentalidade hands on, e tenho experiência como
+        Quality Assurance, e também tenho conhecimentos como Desenvolvedor web FrontEnd, BackEnd e Mobile
+        Full Stack gosto de me envolver em projetos de vários setores e quero
+        fazer parte do seu time.
       </p>
     </section>
   );
