20c17d31ca61254801799f3634f0d782cc9f48a5
Carlos Fermino <brutosacf@gmail.com>
2024-12-06 18:31:25 -0300

    Correção no campo sobre.

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
       <h2>Sobre</h2>
       <p>
         Sou apaixonado por tecnologia com uma mentalidade hands on, e tenho experiência como
-        Quality Assurance, e também tenho conhecimentos como Desenvolvedor web FrontEnd, BackEnd e Mobile
+        Quality Assurance, e também tenho conhecimentos como Desenvolvedor web FrontEnd, BackEnd, Mobile e
         Full Stack gosto de me envolver em projetos de vários setores e quero
         fazer parte do seu time.
       </p>
